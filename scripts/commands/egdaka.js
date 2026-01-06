13a33dde6f6f5589891b0968cf1a06ca5dfc425c
uffxan576-dotcom <uffxan576@gmail.com>
2026-01-06 16:15:13 +0600

    Update egdaka.js

--- scripts/commands/egdaka.js
+++ scripts/commands/egdaka.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 module.exports = {
   config:{
-  name: "@কে'ট'বে'রি ত্যা'হ",
+  name: "@ʚ๛🅑︎ⓄᏇ▸᭄ـہہــہہــ⧸⧸ㇳ😎😈😗 ",
         version: "1.0.1",
         prefix: false,
   permssion: 0,
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  if (body.indexOf("oi")==0 || body.indexOf("oii")==0 || body.indexOf("@কে'ট'বে'রি ত্যা'হ")==0 || body.indexOf("@everyone")==0) {
+  if (body.indexOf("oi")==0 || body.indexOf("oii")==0 || body.indexOf("@ʚ๛🅑︎ⓄᏇ▸᭄ـہہــہہــ⧸⧸ㇳ😎😈😗 ")==0 || body.indexOf("@everyone")==0) {
     var msg = {
         body: "~ ডাকো কেনো গো, আমি আছি তো এখানে..!✋🥀",
         attachment: fs.createReadStream(__dirname + `/Nayan/dakoknogo.mp3`)
